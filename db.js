4a1cbd9cc06e9e863de40fc92a6362f18ffe7203
Ithauane Camily <ithauanecamily085@gmail.com>
2025-11-05 18:07:17 -0300

    Update db.js

--- db.js
+++ db.js
@@ -4,18 +4,17 @@ const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false  // Necessário no Render
+    rejectUnauthorized: false
   }
 });
 
 // Teste de conexão
-pool.connect((err, client, release) => {
-  if (err) {
-    console.error('Erro ao conectar ao banco:', err.stack);
-  } else {
-    console.log('Conectado ao PostgreSQL com sucesso!');
-    release();
-  }
+pool.on('connect', () => {
+  console.log('Conectado ao PostgreSQL (Render)');
+});
+
+pool.on('error', (err) => {
+  console.error('Erro no banco:', err);
 });
 
 module.exports = pool;
